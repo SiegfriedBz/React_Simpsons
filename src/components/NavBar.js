5cf3f9cc810c8c54f88390e800b8173b1591cdc1
siegfried bozza <siegfried.bozza@protonmail.com>
2022-06-15 22:24:42 +0200

    upstyled components bis

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -6,7 +6,7 @@ const NavBar = () => {
   return (
       <nav className="navbar sticky-top navbar-expand-lg bg-light">
         <div className="container-fluid px-5">
-          <h1>Simpsons</h1>
+          <h1 className='text-primary'>Simpsons</h1>
           <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
           </button>
@@ -15,7 +15,7 @@ const NavBar = () => {
             </ul>
             <form className="d-flex" role="search">
               <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
-              <button className="btn btn-outline-success" type="submit"><h4>Search</h4></button>
+              <button className="btn btn-outline-primary" type="submit"><h4>Search</h4></button>
             </form>
           </div>
         </div>
